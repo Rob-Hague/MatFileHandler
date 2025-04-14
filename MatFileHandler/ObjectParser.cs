d24f3c7250fd98f3799bc5d9a51128b7724c770a
Alexander Luzgarev <mahalex@gmail.com>
2025-04-14 18:24:34 +0200

    More file-scoped namespaces

--- MatFileHandler/ObjectParser.cs
+++ MatFileHandler/ObjectParser.cs
@@ -1,28 +1,27 @@
-namespace MatFileHandler
+namespace MatFileHandler;
+
+/// <summary>
+/// Parser for object data.
+/// </summary>
+internal static class ObjectParser
 {
     /// <summary>
-    /// Parser for object data.
+    /// Parse object data.
     /// </summary>
-    internal static class ObjectParser
+    /// <param name="uintArray">Opaque link array.</param>
+    /// <param name="subsystemData">Current subsystem data.</param>
+    /// <returns>Parsed object.</returns>
+    public static IArray ParseObject(MatNumericalArrayOf<uint> uintArray, SubsystemData subsystemData)
     {
-        /// <summary>
-        /// Parse object data.
-        /// </summary>
-        /// <param name="uintArray">Opaque link array.</param>
-        /// <param name="subsystemData">Current subsystem data.</param>
-        /// <returns>Parsed object.</returns>
-        public static IArray ParseObject(MatNumericalArrayOf<uint> uintArray, SubsystemData subsystemData)
-        {
-            var (dimensions, indexToObjectId, classIndex) = DataElementReader.ParseOpaqueData(uintArray.Data);
-            return new OpaqueLink(
-                uintArray.Name,
-                string.Empty,
-                string.Empty,
-                dimensions,
-                uintArray,
-                indexToObjectId,
-                classIndex,
-                subsystemData);
-        }
+        var (dimensions, indexToObjectId, classIndex) = DataElementReader.ParseOpaqueData(uintArray.Data);
+        return new OpaqueLink(
+            uintArray.Name,
+            string.Empty,
+            string.Empty,
+            dimensions,
+            uintArray,
+            indexToObjectId,
+            classIndex,
+            subsystemData);
     }
 }
